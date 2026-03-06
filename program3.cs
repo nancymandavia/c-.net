2de101a7135fa8e0e9fac8b5cbca49d7b16aa0fd
nancymandavia <nancymandavia.126465@marwadiuniversity.ac.in>
2026-03-06 19:57:13 +0530

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter value of a");
+            double value = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter value of rate");
+            double rate = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the year");
+            double year = double.Parse(Console.ReadLine());
+            double si = (value + rate + year) / 100;
+            Console.WriteLine("simple interest" + si);
+            
+
+        }
+    }
+}
