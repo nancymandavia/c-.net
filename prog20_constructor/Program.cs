db801387dc1f5ae2a021ebc270228d8d2b0705fa
nancymandavia <nancymandavia.126465@marwadiuniversity.ac.in>
2026-03-23 10:33:43 +0530

    Add files via upload

---
+++ prog20_constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog20_constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectange r1 = new rectange();
+
+
+            r1.calculate();
+            r1.display();
+            rectange r2 = new rectange();
+
+            r2.calculate();
+            r2.display();
+        }
+    }
+}
---
+++ prog20_constructor/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog20_constructor
+{
+    
+        internal class rectange
+        {
+            int l, w, area;
+
+            public rectange()
+            {
+                l = 10;
+                w = 20;
+            }
+            public rectange(int l1, int w1)
+            {
+                l = w1;
+                w = w1;
+            }
+            public void calculate()
+            {
+                area = l * w;
+            }
+            public void display()
+            {
+                Console.WriteLine("area=" + area);
+            }
+        }
+    }
+
+
+
